a9e29f9d7142fa35650c6c6dfa76ee5ac721b722
Steven Hutchings <sthutchi@microsoft.com>
2019-11-07 15:30:38 -0800

    Make action a shell

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const core = require('@actions/core');
+// const core = require('@actions/core');
 
 const { promisify } = require('util');
 const { exec } = require('child_process');
@@ -9,40 +9,40 @@ async function run() {
     try {
         console.log("Starting action.");
         
-        try {
-            await execAsyncInternal(`az --version`);
-            console.log("Azure CLI is available.");
-        } catch (error) {
-            console.log("Unable to find Azure CLI");
-            core.setFailed(error.message);
-            return;
-        }
+    //     try {
+    //         await execAsyncInternal(`az --version`);
+    //         console.log("Azure CLI is available.");
+    //     } catch (error) {
+    //         console.log("Unable to find Azure CLI");
+    //         core.setFailed(error.message);
+    //         return;
+    //     }
 
-        const appSecretsJSON = core.getInput('app_secrets');
-        const functionName = core.getInput('az_func_name');
-        const functionResourceGroup = core.getInput('az_func_resource_group');
-        if (!appSecretsJSON
-            || !functionName
-            || !functionResourceGroup) {
-            core.setFailed("Either of the input value is null or empty.");
-            return;
-        }
+    //     const appSecretsJSON = core.getInput('app_secrets');
+    //     const functionName = core.getInput('az_func_name');
+    //     const functionResourceGroup = core.getInput('az_func_resource_group');
+    //     if (!appSecretsJSON
+    //         || !functionName
+    //         || !functionResourceGroup) {
+    //         core.setFailed("Either of the input value is null or empty.");
+    //         return;
+    //     }
 
-        const secrets = JSON.parse(appSecretsJSON);
-        for (var key in secrets) {
-            if (secrets.hasOwnProperty(key)) {
-                try {
-                    await execAsyncInternal(`az functionapp config appsettings set --settings ${key}=${secrets[key]} --resource-group ${functionResourceGroup} --name ${functionName}`);
-                    console.log("Added the secret to app setting.");
-                } catch (error) {
-                    console.log("Failed to add secrets to app setting.");
-                    core.setFailed(error.message);
-                    return;
-                }
-            }
-        }
+    //     const secrets = JSON.parse(appSecretsJSON);
+    //     for (var key in secrets) {
+    //         if (secrets.hasOwnProperty(key)) {
+    //             try {
+    //                 await execAsyncInternal(`az functionapp config appsettings set --settings ${key}=${secrets[key]} --resource-group ${functionResourceGroup} --name ${functionName}`);
+    //                 console.log("Added the secret to app setting.");
+    //             } catch (error) {
+    //                 console.log("Failed to add secrets to app setting.");
+    //                 core.setFailed(error.message);
+    //                 return;
+    //             }
+    //         }
+    //     }
     } catch (error) {
-        core.setFailed(error.message);
+        console.log("Failed");
     }
 }
 
