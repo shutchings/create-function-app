dcb972ea6a6c2ffe0b0d2d6f981c4fe9a2a93f8d
Steven Hutchings <sthutchi@microsoft.com>
2019-11-07 16:56:31 -0800

    add all az cli commands

--- index.js
+++ index.js
@@ -8,75 +8,113 @@ const execAsyncInternal = promisify(exec);
 const subscriptionId = "4f670563-09a7-43dd-a8f8-334946660e1e";
 const resourceGroupName = "action-testing";
 const location = "centralus";
+const storageAccountName = "shactiontestingsa";
+const storageContainerName = "shactioncontainer";
+const functionAppname = "action-testing-app";
+
+function fail(error, message) {
+    console.log(message)
+    core.setFailed(error.message);
+
+    return;
+}
 
 async function run() {
     try {
-        console.log("Starting action.");
-        
-        try {
-            await execAsyncInternal(`az --version`);
-            console.log("Azure CLI is available.");
-        } catch (error) {
-            console.log("Unable to find Azure CLI");
-            core.setFailed(error.message);
-            return;
-        }
-
-        await createResourceGroup();
-
-    //     const appSecretsJSON = core.getInput('app_secrets');
-    //     const functionName = core.getInput('az_func_name');
-    //     const functionResourceGroup = core.getInput('az_func_resource_group');
-    //     if (!appSecretsJSON
-    //         || !functionName
-    //         || !functionResourceGroup) {
-    //         core.setFailed("Either of the input value is null or empty.");
-    //         return;
-    //     }
-
-    //     const secrets = JSON.parse(appSecretsJSON);
-    //     for (var key in secrets) {
-    //         if (secrets.hasOwnProperty(key)) {
-    //             try {
-    //                 await execAsyncInternal(`az functionapp config appsettings set --settings ${key}=${secrets[key]} --resource-group ${functionResourceGroup} --name ${functionName}`);
-    //                 console.log("Added the secret to app setting.");
-    //             } catch (error) {
-    //                 console.log("Failed to add secrets to app setting.");
-    //                 core.setFailed(error.message);
-    //                 return;
-    //             }
-    //         }
-    //     }
+        await execAsyncInternal(`az --version`);
+        console.log("Azure CLI is available.");
     } catch (error) {
-        console.log("Failed");
+        fail("Unable to find Azure CLI");
+        return;
     }
-}
 
-async function createResourceGroup() {
     try {
         console.log(`Creating resource group ${resourceGroupName}`);
 
         await execAsyncInternal(
-          `az group exists --subscription ${subscriptionId} --name ${resourceGroupName}`
-        ).then(exists => {
-            var propValue;
-            for(var propName in exists) {
-                propValue = exists[propName]
-
-                console.log(propName,propValue);
-            }
-            console.log(`Exists: ${exists}`);
-
-            if (exists.stdout.search("true")) {
-                console.log(`Resource group already exists`);
-            }
-        });
-        await execAsyncInternal(
-          `az group create --subscription ${subscriptionId} --name ${resourceGroupName} --location ${location}`
+            `az group create \
+            --subscription ${subscriptionId} \
+            --name ${resourceGroupName} \
+            --location ${location}`
         );
-    } catch (e) {
-        console.log("Could not create resource group")
-        console.log(e);
+    } catch (error) {
+        fail("Unable to create resource groupo");
+        return;
+    }
+    
+    try {
+        console.log(`Creating storage account ${storageAccountName}`);
+
+        await execAsyncInternal(
+            `az storage account create \
+            --subscription ${subscriptionId} \
+            --resource-group ${resourceGroupName} \
+            --name ${storageAccountName} \
+            --location ${location} \
+            --sku Standard_LRS`
+        );
+    } catch (error) {
+        fail("Unable to create storage account");
+        return;
+    }
+
+    try {
+        console.log(`Creating storage container ${storageContainerName}`);
+
+        await execAsyncInternal(
+            `az storage container create \
+            --subscription ${subscriptionId} \
+            --name ${storageContainerName} \
+            --acount-name ${storageAccountName}`
+        );
+    } catch (error) {
+        fail("Unable to create storage container");
+        return;
+    }
+
+    try {
+        console.log(`Setting storage container permissions`);
+
+        await execAsyncInternal(
+            `az storage container set-permission \
+            --public-access blob \
+            --subscription ${subscriptionId} \
+            --account-name ${storageAccountName} \
+            --name ${storageContainerName}`
+        );
+    } catch (error) {
+        fail("Unable to set storage container permissions");
+        return;
+    }
+
+    try {
+        console.log(`Creating function app ${functionAppName}`);
+
+        await execAsyncInternal(
+            `az functionapp create \
+            --subscription ${subscriptionId} \
+            --resource-group ${resourceGroupName} \
+            --consumption-plan-location ${location} \
+            --name ${functionAppname} \
+            --storage-account ${storageAccount} --runtime node`
+        );
+    } catch (error) {
+        fail("Unable to create function app");
+        return;
+    }
+
+    try {
+        console.log(`Enabling package deploy`);
+
+        await execAsyncInternal(
+            `az functionapp config appsettings set \
+            --settings WEBSITE_RUN_FROM_PACKAGE=1 \
+            --resource-groupo ${resourceGroup} \
+            --name ${name}`
+        );
+    } catch (error) {
+        fail("Could not enable package deployment");
+        return;
     }
 }
 
